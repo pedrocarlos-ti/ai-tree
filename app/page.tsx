0417950f463c07548abbb2cdb631035c1609ef28
Pedro Santos <pedrocarlos.ti@gmail.com>
2024-11-12 19:48:57 +0000

    feat: add tailwind theme and fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 const Home = () => {
   return (
-    <div className="flex items-center justify-center h-screen">
-      Hello tree 🌳
+    <div className="flex h-screen flex-col items-center justify-center">
+      <h1 className="text-7xl font-bold">Welcome to the tree🌳</h1>
+      <h1 className="font-space-grotesk text-7xl font-bold">
+        Welcome to the tree 🌳
+      </h1>
     </div>
   );
 };
